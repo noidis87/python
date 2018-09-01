3a384b71dd17611b306e3d3dfd03ae7ff166a7be
emato <emato@uci.cu>
2018-09-01 02:20:21 +0200

    cvbb

--- tarea_final/__manifest__.py
+++ tarea_final/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Tarea final',
+    'name': 'Tprobandoo',
     'version': '1.0',
     'summary': 'Tarea final',
     'description': """Tarea final""",
